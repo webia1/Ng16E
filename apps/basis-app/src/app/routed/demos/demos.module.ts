765a8f661727a88cb8eebb904dfdfe95c9ec027c
webia1 <webia1.github@gmail.com>
2023-09-07 17:26:26 +0200

    Angular Material Examples

--- apps/basis-app/src/app/routed/demos/demos.module.ts
+++ apps/basis-app/src/app/routed/demos/demos.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { DemosRoutingModule } from './demos-routing.module';
 import { MaterialExampleComponent } from './components/material-example/material-example.component';
 import { FormlyExampleComponent } from './components/formly-example/formly-example.component';
+import { CustomMaterialModule } from '@app-shared-libs/material';
 
 @NgModule({
   declarations: [MaterialExampleComponent, FormlyExampleComponent],
-  imports: [CommonModule, DemosRoutingModule],
+  imports: [CommonModule, DemosRoutingModule, CustomMaterialModule],
 })
 export class DemosModule {}
