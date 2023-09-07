672bd99fd868c54f8503afd3d428714b49bd0992
webia1 <webia1.github@gmail.com>
2023-09-07 15:09:49 +0200

    Shared/Routed Modules

--- apps/basis-app/src/app/app.module.ts
+++ apps/basis-app/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
+import { DemosModule } from './routed/modules/demos/demos.module';
+import { FormlyModule } from './shared/modules/formly/formly.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+    DemosModule,
+    FormlyModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ apps/basis-app/src/app/shared/modules/formly/formly.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FormlyRoutingModule } from './formly-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FormlyRoutingModule
+  ]
+})
+export class FormlyModule { }
