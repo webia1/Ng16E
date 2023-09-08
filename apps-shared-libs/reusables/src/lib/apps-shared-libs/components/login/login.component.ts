06d8c374f1b71a6257522b9a911f5570124ce52b
webia1 <webia1.github@gmail.com>
2023-09-08 17:23:41 +0200

    Simple Login Form as Shared Library - Simple Form created

--- apps-shared-libs/reusables/src/lib/apps-shared-libs/components/login/login.component.ts
+++ apps-shared-libs/reusables/src/lib/apps-shared-libs/components/login/login.component.ts
@@ -1,3 +1,5 @@
+import { FormlyFieldConfig } from '@ngx-formly/core';
+import { FormGroup } from '@angular/forms';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,4 +7,32 @@ import { Component } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  form = new FormGroup({});
+  model = { username: '', password: '' };
+  fields: Array<FormlyFieldConfig> = [
+    {
+      key: 'username',
+      type: 'input',
+      templateOptions: {
+        label: 'Username',
+        placeholder: 'Enter username',
+        required: true,
+      },
+    },
+    {
+      key: 'password',
+      type: 'input',
+      templateOptions: {
+        type: 'password',
+        label: 'Password',
+        placeholder: 'Enter password',
+        required: true,
+      },
+    },
+  ];
+
+  submit(model: unknown) {
+    console.log('Model: ', model);
+  }
+}
--- apps-shared-libs/reusables/src/lib/reusables.module.ts
+++ apps-shared-libs/reusables/src/lib/reusables.module.ts
@@ -1,9 +1,12 @@
+import { CustomMaterialModule } from '@app-shared-libs/material';
+import { CustomFormlyModule } from '@app-shared-libs/formly';
+
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './apps-shared-libs/components/login/login.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, CustomMaterialModule, CustomFormlyModule],
   declarations: [LoginComponent],
 })
 export class AppsSharedLibsReusablesModule {}
