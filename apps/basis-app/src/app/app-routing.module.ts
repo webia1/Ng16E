cb08e4e412c0db3c1483c8dd308c291d7cfe2507
webia1 <webia1.github@gmail.com>
2023-09-10 17:55:21 +0200

    Login Example Route - Navigates Dashboard

--- apps/basis-app/src/app/app-routing.module.ts
+++ apps/basis-app/src/app/app-routing.module.ts
@@ -1,6 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-const routes: Routes = [{ path: 'dashboard', loadChildren: () => import('./routed/dashboard/dashboard.module').then(m => m.DashboardModule) }];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'demos' },
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./routed/dashboard/dashboard.module').then(
+        (m) => m.DashboardModule
+      ),
+  },
+  {
+    path: 'demos',
+    loadChildren: () =>
+      import('./routed/demos/demos.module').then((m) => m.DemosModule),
+  },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
