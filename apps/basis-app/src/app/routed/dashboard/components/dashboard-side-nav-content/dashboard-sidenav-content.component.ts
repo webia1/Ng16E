4a07adb26fbdf28d226f25e9a6960504529e8bba
webia1 <webia1.github@gmail.com>
2023-09-12 17:05:06 +0200

    Main Content Drawer

--- apps/basis-app/src/app/routed/dashboard/components/dashboard-side-nav-content/dashboard-sidenav-content.component.ts
+++ apps/basis-app/src/app/routed/dashboard/components/dashboard-side-nav-content/dashboard-sidenav-content.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-sidenav-content',
   templateUrl: './dashboard-sidenav-content.component.html',
   styleUrls: ['./dashboard-sidenav-content.component.scss'],
 })
-export class DashboardSidenavContentComponent {}
+export class DashboardSidenavContentComponent {
+  @Input() leftSideNavOpen;
+  @Input() rightSideNavOpen;
+}
