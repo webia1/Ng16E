ea51b02cf45e829ff80137e3385738301b7e73cd
webia1 <webia1.github@gmail.com>
2023-09-14 21:09:14 +0200

    HMR Branch

--- apps/basis-app/src/main.ts
+++ apps/basis-app/src/main.ts
@@ -1,6 +1,26 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch((err) => console.error(err));
+interface HotModule {
+  accept: () => void;
+  dispose: (callback: () => void) => void;
+}
+declare let module: { hot: HotModule };
+const bootstrap = () => platformBrowserDynamic().bootstrapModule(AppModule);
+
+if (module['hot']) {
+  module['hot'].accept();
+  module['hot'].dispose(() => {
+    /**
+     * Possible Szenarios:
+     * - Deleting Intervals/Timeouts
+     * - Removal of some eventlisteners
+     * - Closing of WebSockets,..
+     */
+  });
+  bootstrap().then((mod) => {
+    console.log ('MOD is loaded!', mod);
+  });
+} else {
+  bootstrap().catch((err) => console.error(err));
+}
